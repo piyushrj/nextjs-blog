abd767bdb81ab36a369374db3a9d37b15c16b838
Piyush Raj <piyushrajiitr@gmail.com>
2022-11-10 23:54:56 +0530

    added post functionality, posts stored as md files in the filesystem

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
 import { Fragment } from "react";
+import FeaturedPosts from "../components/home-page/featured-posts";
 
 import Hero from "../components/home-page/hero";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-function HomePage() {
+function HomePage(props) {
     return (
         <Fragment>
             <Hero />
+            <FeaturedPosts posts={props.posts} />
         </Fragment>
     )
+}
+
+export function getStaticProps() {
+    const featuredPosts = getFeaturedPosts();
+    return {
+        props: {
+            posts: featuredPosts
+        }
+    }
 }
 
 export default HomePage;
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,3 +1,32 @@
-function SinglePostPage() { }
+import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
+
+function SinglePostPage(props) {
+    return <PostContent post={props.post} />
+}
+
+export function getStaticProps(context) {
+    const { params } = context;
+    const { slug } = params;
+
+    const postData = getPostData(slug);
+
+    return {
+        props: {
+            post: postData
+        },
+        revalidate: 600
+    }
+}
+
+export function getStaticPaths() {
+    const postFiles = getPostsFiles();
+    const slugs = postFiles.map(fileName => fileName.replace(/\.md$/, ""));
+
+    return {
+        paths: slugs.map((slug) => ({ params: { slug: slug } })),
+        fallback: false
+    };
+}
 
 export default SinglePostPage;
